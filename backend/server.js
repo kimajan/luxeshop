1e323e8854b9380ae88adce42061004318087885
Kim <nativetek1@gmail.com>
2020-12-16 19:47:11 -0800

    prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5000;
+app.set("port", port);
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
