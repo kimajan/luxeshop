781ff74a69efed3d7b26d029ee3d86391363f784
Kim <nativetek1@gmail.com>
2020-12-16 20:28:10 -0800

    prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,11 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+const __dirnme = path.resolve();
+app.use('/uploads', express.static(path.join(__dirnme, '/uploads')));
+app.use(express.static(path.join(__dirnme, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirnme, '/frontend/build/index.html'))
 );
 
 
